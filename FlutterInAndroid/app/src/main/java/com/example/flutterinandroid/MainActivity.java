4c987b912853f4d570e73ecbbaa66b4c6338e2b1
wx178660 <wx178660@alibaba-inc.com>
2019-02-18 14:56:09 +0800

    init flutter

--- FlutterInAndroid/app/src/main/java/com/example/flutterinandroid/MainActivity.java
+++ FlutterInAndroid/app/src/main/java/com/example/flutterinandroid/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.flutterinandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import io.flutter.facade.Flutter;
+import io.flutter.view.FlutterView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //防止黑屏，保留setContentView
         setContentView(R.layout.activity_main);
+
+        FlutterView flutterView = Flutter.createView(this, getLifecycle(), "route1");
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT
+                , FrameLayout.LayoutParams.MATCH_PARENT);
+        addContentView(flutterView, layoutParams);
     }
 }
